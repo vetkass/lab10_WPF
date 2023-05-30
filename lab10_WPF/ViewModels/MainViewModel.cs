0aabd906a7ed638ea91955bce74ebeff5cf81803
SvetaGlobal <s.chernova@globalbit.co.il>
2023-05-30 18:25:14 +0200

    add buttons for window

--- lab10_WPF/ViewModels/MainViewModel.cs
+++ lab10_WPF/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using System.Windows;
 
 namespace lab10_WPF.ViewModels
 {
@@ -28,6 +29,49 @@ namespace lab10_WPF.ViewModels
         }
         #endregion
 
+        #region Commands
+
+        #region Drag Move Command Action
+        /// <summary>
+        /// allows to move the window when the style is none
+        /// </summary>
+        /// <param name="parameter">the indow itself here/param>
+        public void DragMove(object parameter)
+        {
+            if (parameter is Window window)
+            {
+                window.DragMove();
+            }
+        }
+        #endregion
+
+        #region Close Application Command Action
+        /// <summary>
+        /// allows to close the window
+        /// </summary>
+        public void CloseWindow()
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #region Collapse Application Command Action
+        /// <summary>
+        /// allows to collapse the window
+        /// </summary>
+        public void CollapseWindow(object parameter)
+        {
+            if (parameter is Window window)
+            {
+                window.WindowState=WindowState.Minimized;
+            }
+        }
+        #endregion
+
+
+
+
+        #endregion
         public MainViewModel()
         {
         }
